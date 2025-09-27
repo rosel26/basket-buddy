75ebb348c00c79403f08d98232bf1c1368e19e6b
rosel26 <roseliu@andrew.cmu.edu>
2025-09-26 20:11:44 -0400

    Initial Commit, First Cart View

---
+++ BasketBuddy/BasketBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketBuddyApp.swift
+//  BasketBuddy
+//
+//  Created by Rose Liu on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct BasketBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CartControlView()
+        }
+    }
+}
---
+++ BasketBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasketBuddy
+//
+//  Created by Rose Liu on 2025-09-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
